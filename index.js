efe384f0643240cba7bc56487e603722ee8e1fe0
pzcuong <dok2308@gmail.com>
2022-09-24 20:29:12 +0700

    Fix insert thông tin

--- index.js
+++ index.js
@@ -47,66 +47,74 @@ app.get('/dangky/:vitri', (req, res) => {
 });
 
 app.post('/dangky/:vitri', async (req, res) => {
-    let vitri = req.params.vitri;
-    let data = req.body;
-    let spreadsheetId = "1GgsKb5WksC1SOLcbq5V-PmEJVHw22asw7mSOf6ufGd8";
-    if(vitri != 'Training' && vitri != 'TruyenThong' && vitri != 'Khac')
-        res.redirect('/');
-    let dataSheets = await spreadsheetsModels.getSpreadsheet(spreadsheetId, "A:F");
-    for (value in dataSheets) {
-        if(dataSheets[value].at(0) == data.email) 
+    try {
+        let vitri = req.params.vitri;
+        let data = req.body;
+        let spreadsheetId = "1GgsKb5WksC1SOLcbq5V-PmEJVHw22asw7mSOf6ufGd8";
+        if(vitri != 'Training' && vitri != 'TruyenThong' && vitri != 'Khac')
+            res.redirect('/');
+        let dataSheets = await spreadsheetsModels.getSpreadsheet(spreadsheetId, "A:F");
+        for (value in dataSheets) {
+            if(dataSheets[value].at(0) == data.email) 
+                return res.json({
+                    status: 400,
+                    message: "Email đã tồn tại"
+                });
+            if(dataSheets[value].at(2) == data.mssv) 
+                return res.json({
+                    status: 400,
+                    message: "Mã số sinh viên đã tồn tại"
+                });
+        }
+        console.log(data);
+    
+        if(!data.name || !data.email || !data.mssv || !data.LopSV || !data.FacebookURL || !data.LyDoThamGia || !data.TinhCachMuonLamViec || !data.TinhCachKhongMuonLamViec || !data.TinhHuong || !data.CauHoi2)
             return res.json({
-                status: 400,
-                message: "Email đã tồn tại"
+                success: false,
+                message: "Vui lòng điền đầy đủ thông tin"
             });
-        if(dataSheets[value].at(2) == data.mssv) 
+    
+        //Insert to Tổng quan
+        spreadsheetsModels.insertSpreadsheet(spreadsheetId, "'Tổng quan'!A:F", [[
+            data.email,
+            data.name,
+            data.mssv,
+            data.LopSV,
+            req.params.vitri,
+            data.FacebookURL
+        ]]);
+        //Insert to Training
+        let dataInsert = [[
+            data.mssv,
+            data.LyDoThamGia,
+            data.TinhCachMuonLamViec,
+            data.TinhCachKhongMuonLamViec,
+            data.TinhHuong,
+            data.TinhHuongTraining
+        ]];
+        if(req.params.vitri == 'Training') {
+            await spreadsheetsModels.insertSpreadsheet(spreadsheetId, "'Training'!A:F", dataInsert);
+        } else if(req.params.vitri == 'TruyenThong') {
+            await spreadsheetsModels.insertSpreadsheet(spreadsheetId, "'Truyền thông'!A:F", dataInsert);
+        } else if(req.params.vitri == 'Khac') {
+            await spreadsheetsModels.insertSpreadsheet(spreadsheetId, "'Khác'!A:F", dataInsert);
+        } else {
             return res.json({
-                status: 400,
-                message: "Mã số sinh viên đã tồn tại"
+                success: false,
+                message: "Vị trí không hợp lệ"
             });
-    }
-    console.log(data);
-
-    if(!data.name || !data.email || !data.mssv || !data.LopSV || !data.FacebookURL || !data.LyDoThamGia || !data.TinhCachMuonLamViec || !data.TinhCachKhongMuonLamViec || !data.TinhHuong || !data.CauHoi2)
+        }
+        return res.json({
+            success: true,
+            message: "Đăng ký thành công"
+        });
+    } catch (error) {
+        console.log(error);
         return res.json({
             success: false,
-            message: "Vui lòng điền đầy đủ thông tin"
-        });
-
-    //Insert to Tổng quan
-    spreadsheetsModels.insertSpreadsheet(spreadsheetId, "'Tổng quan'!A:F", [[
-        data.email,
-        data.name,
-        data.mssv,
-        data.LopSV,
-        req.params.vitri,
-        data.FacebookURL
-    ]]);
-    //Insert to Training
-    let dataInsert = [[
-        data.mssv,
-        data.LyDoThamGia,
-        data.TinhCachMuonLamViec,
-        data.TinhCachKhongMuonLamViec,
-        data.TinhHuong,
-        data.TinhHuongTraining
-    ]];
-    if(req.params.vitri == 'Training') {
-        spreadsheetsModels.insertSpreadsheet(spreadsheetId, "'Training'!A:F", dataInsert);
-    } else if(req.params.vitri == 'TruyenThong') {
-        spreadsheetsModels.insertSpreadsheet(spreadsheetId, "'Truyền thông'!A:F", dataInsert);
-    } else if(req.params.vitri == 'Khac') {
-        spreadsheetsModels.insertSpreadsheet(spreadsheetId, "'Khác'!A:F", dataInsert);
-    } else {
-        return res.json({
-            success: false,
-            message: "Vị trí không hợp lệ"
+            message: "Đăng ký thất bại"
         });
     }
-    return res.json({
-        success: true,
-        message: "Đăng ký thành công"
-    });
 });
 
 app.use((req, res, next) => {
